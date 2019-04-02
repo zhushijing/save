938f18b3e5e196b0f2fe35d1cf3b689f55c895b0
zhushijing <48074959+zhushijing@users.noreply.github.com>
2019-04-02 10:32:36 +0800

    Add files via upload

--- untitled.m
+++ untitled.m
@@ -1,5 +1,5 @@
 function varargout = untitled(varargin)
-% UNTITLED M-file for untitled.fig
+% UNTITLED MATLAB code for untitled.fig
 %      UNTITLED, by itself, creates a new UNTITLED or raises the existing
 %      singleton*.
 %
@@ -11,7 +11,7 @@ function varargout = untitled(varargin)
 %
 %      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before untitled_OpeningFunction gets called.  An
+%      applied to the GUI before untitled_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
 %      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
 %
@@ -22,7 +22,7 @@ function varargout = untitled(varargin)
 
 % Edit the above text to modify the response to help untitled
 
-% Last Modified by GUIDE v2.5 19-Mar-2019 15:49:23
+% Last Modified by GUIDE v2.5 02-Apr-2019 10:26:33
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -51,7 +51,7 @@ function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to untitled (see VARARGIN)
-set(handles,canshu,'string','   ')
+
 % Choose default command line output for untitled
 handles.output = hObject;
 
@@ -73,63 +73,30 @@ function varargout = untitled_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in bedign.
-function bedign_Callback(hObject, eventdata, handles)
-% hObject    handle to bedign (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-a={'apple','banana','orange','peach','pear'};
-b=ceil(rand(1,5)*20);
-% 排序，绘图，然后修改TickLable
-[b,idx]=sort(b(:),1,'descend');
-a=a(idx);
-bar(1:length(a),b)
-
-% --- Executes on button press in close.
-function close_Callback(hObject, eventdata, handles)
-% hObject    handle to close (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-close
-
-% --- Executes on button press in word_list.
-function word_list_Callback(hObject, eventdata, handles)
-% hObject    handle to word_list (see GCBO)
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 word=fileread('C:\Users\Administrator\Desktop\234.txt'); %读入全文
 word=regexprep(word,'\W',' '); %不是字符的，都转换为空格。主要是去除标点符号
 word=lower(word); %变成小写
 words=regexp(word,' ','split')'; %根据空格分隔为单词cell
-
 %至此每个单词都拿出来了
-
 rank = tabulate(words); %rank是三列向量，包括名称，出现次数和百分比
-ans=sortrows(rank,-2); %只根据第二列进行排序 -2表示降序
- 
+ans=sortrows(rank,-3); %只根据第二列进行排序 -2表示降序
+a=ans;
+b=ceil(rand(1,5)*20);
+% 排序，绘图，然后修改TickLable
+[b,idx]=sort(b(:),1,'descend');
+a=a(idx);
+bar(1:length(a),b)
+set(gca,'xticklabel',a)    
  xlswrite('results',ans);%输出为excel文件
 
-
-
-function edit1_Callback(hObject, eventdata, handles)
-% hObject    handle to edit1 (see GCBO)
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit1 as text
-%        str2double(get(hObject,'String')) returns contents of edit1 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
+close
